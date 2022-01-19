ca2286f6108c8326600d269a7af46c9287118bb9
zukamazana <zurabi.mazanashvili.1@btu.edu.ge>
2022-01-18 17:17:41 -0800

    fix http protocol error

--- src/api/questions.js
+++ src/api/questions.js
@@ -1,5 +1,5 @@
 const url =
-  "http://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db";
+  "https://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db";
 
 export default async function getQuestions() {
   return await fetch(url).then((res) => res.json());
